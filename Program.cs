8281200164e434f33f748e8bf329d8ab34bdd519
Leminhquan1601 <leminhquan16022009@gmail.com>
2025-07-25 00:04:07 +0700

    add
    add

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        const int rate = 23000;
+        Console.WriteLine("Nhap gia tien do");
+        double USD = Convert.ToDouble(Console.ReadLine());
+        double VND = USD * rate;
+        Console.WriteLine(VND);
+    }
+}
